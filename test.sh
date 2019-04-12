41118f47818d64d889a1cabf6f32afa05ef128d5
Helena Rasche <hxr@hx42.org>
2019-04-12 12:07:57 +0200

    add some more tests

--- test.sh
+++ test.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bats
 
+@test "Ensure gxadmin exits with zero" {
+	run ./gxadmin
+	[ "$status" -eq 0 ]
+}
+
+@test "Ensure help is not too long" {
+	out_lines=$(./gxadmin | wc -l)
+	[ "$out_lines" -lt 100 ]
+}
+
+@test "Version should be returned" {
+	out_lines=$(./gxadmin version)
+	[ "$out_lines" -gt 0 ]
+}
+
 @test "Ensure no trailing semicolons in queries" {
 	result=$(grep -Pzl '(?s);\n\tEOF' parts/* | wc -l)
 	if (( $result > 0 )); then
