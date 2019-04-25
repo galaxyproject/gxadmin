6247ba9834fd74a5d793380c68e8eb1d36d3b5d1
Helena Rasche <hxr@hx42.org>
2019-04-25 11:30:57 +0200

    more helpful test messages

--- test.sh
+++ test.sh
@@ -16,7 +16,7 @@
 	[ "$out_lines" -gt 0 ]
 }
 
-@test "Ensure no trailing semicolons in queries" {
+@test "Ensure no trailing semicolons in queries (fix: remove semicolon from before EOF)" {
 	result=$(grep -Pzl '(?s);\n\tEOF' parts/* | wc -l)
 	if (( $result > 0 )); then
 		grep -Pzl '(?s);\n\t*EOF' parts/*
@@ -24,12 +24,16 @@
 	[ "$result" -eq 0 ]
 }
 
-@test "Ensure that changes were made to BOTH parts/* and gxadmin" {
+@test "Ensure that changes were made to BOTH parts/* and gxadmin (fix: run make)" {
 	run diff <(cat gxadmin) <(cat parts/*.sh)
 	[ "$status" -eq 0 ]
 }
 
 @test "Ensure only using tabs" {
 	out_lines=$(egrep '^ +[^\t]' gxadmin | grep -v '%s' | wc -l)
-	[ "$out_lines" -lt 5 ]
+	if (( $out_lines > 2 )); then
+		echo "Lines with potential issues. Either fix these or increase the exclusion in the test.sh if it is valid (e.g. inside printf)"
+		egrep '^ +[^\t]' parts/* | grep -v %s
+	fi
+	[ "$out_lines" -lt 3 ]
 }
