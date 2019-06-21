148432b4f3e2e2bfa3ed3b2a976011a78fac4195
Helena Rasche <hxr@hx42.org>
2019-06-21 18:29:31 +0200

    done

--- parts/22-query.sh
+++ parts/22-query.sh
@@ -1564,37 +1564,41 @@ query_tool-new-errors() { ## [weeks|4]: Summarize percent of tool runs in error 
 	handle_help "$@" <<-EOF
 		See jobs-in-error summary for recent tools (tools whose first execution is in recent weeks).
 
-		    $ gxadmin tool-new-errors 4
-		                            tool_id                                                  | tool_runs | percent_failed_errored | percent_failed
-		    ---------------------------------------------------------------------------------+-----------+------------------------+----------------
-		     toolshed.g2.bx.psu.edu/repos/bgruening/bismark/bismark_bowtie2/0.7.12           |        16 |   100.0000000000000000 |              0
-		     toolshed.g2.bx.psu.edu/repos/devteam/samtools_phase/samtools_phase/2.0.1        |        20 |   100.0000000000000000 |              0
-		     toolshed.g2.bx.psu.edu/repos/iuc/snpsift/snpSift_rmInfo/4.3+t.galaxy0           |         1 |   100.0000000000000000 |              0
-		     toolshed.g2.bx.psu.edu/repos/devteam/principal_component_analysis/pca1/1.0.2    |       177 |    98.8700564971751412 |              0
-		     toolshed.g2.bx.psu.edu/repos/bgruening/bismark/bismark_bowtie/0.7.12            |       225 |    96.0000000000000000 |              0
-		     toolshed.g2.bx.psu.edu/repos/devteam/picard/PicardHsMetrics/1.56.0              |        41 |    95.1219512195121951 |              0
-		     toolshed.g2.bx.psu.edu/repos/iuc/stacks_procrad/stacks_procrad/1.46.0           |      3288 |    94.0389294403892944 |              0
-		     toolshed.g2.bx.psu.edu/repos/iuc/jcvi_gff_stats/jcvi_gff_stats/0.8.4            |        16 |    93.7500000000000000 |              0
-		     genomespace_exporter                                                            |        72 |    93.0555555555555556 |              0
-		     toolshed.g2.bx.psu.edu/repos/devteam/ncbi_blast_plus/ncbi_tblastn_wrapper/0.3.1 |      1638 |    86.7521367521367521 |              0
-		     toolshed.g2.bx.psu.edu/repos/iuc/samtools_sort/samtools_rmdup/1.0.1             |       355 |    86.4788732394366197 |              0
-		     toolshed.g2.bx.psu.edu/repos/iuc/bcftools_call/bcftools_call/1.9                |         7 |    85.7142857142857143 |              0
-		     toolshed.g2.bx.psu.edu/repos/iuc/stacks_denovomap/stacks_denovomap/1.46.0       |       214 |    83.6448598130841121 |              0
-		     toolshed.g2.bx.psu.edu/repos/iuc/intervene/intervene_upset/0.6.4                |        11 |    81.8181818181818182 |              0
-		     toolshed.g2.bx.psu.edu/repos/rnateam/rnalien/RNAlien/1.3.6                      |         5 |    80.0000000000000000 |              0
+		    $ gxadmin query tool-errors --short-tool-id 1
+		        tool_id                        | tool_runs |  percent_errored  | percent_failed | count_errored | count_failed |     handler
+		    -----------------------------------+-----------+-------------------+----------------+---------------+--------------+-----------------
+		     rnateam/graphclust_align_cluster/ |        55 | 0.145454545454545 |              0 |             8 |            0 | handler_main_10
+		     iuc/rgrnastar/rna_star/2.6.0b-2   |        46 | 0.347826086956522 |              0 |            16 |            0 | handler_main_3
+		     iuc/rgrnastar/rna_star/2.6.0b-2   |        43 | 0.186046511627907 |              0 |             8 |            0 | handler_main_0
+		     iuc/rgrnastar/rna_star/2.6.0b-2   |        41 | 0.390243902439024 |              0 |            16 |            0 | handler_main_4
+		     iuc/rgrnastar/rna_star/2.6.0b-2   |        40 |             0.325 |              0 |            13 |            0 | handler_main_6
+		     Filter1                           |        40 |             0.125 |              0 |             5 |            0 | handler_main_0
+		     devteam/bowtie2/bowtie2/2.3.4.3   |        40 |             0.125 |              0 |             5 |            0 | handler_main_7
+		     iuc/rgrnastar/rna_star/2.6.0b-2   |        40 |               0.3 |              0 |            12 |            0 | handler_main_2
 	EOF
 
-	weeks=${1:-4}
+	# TODO: Fix this nonsense for proper args
+	tool_id="j.tool_id"
+	if [[ "$1" = --short-tool-id ]]; then
+		tool_id="regexp_replace(j.tool_id, '.*toolshed.*/repos/', '') as tool_id"
+		weeks=${2:-4}
+	else
+		weeks=${1:-4}
+	fi
 
-	fields="tool_runs=1;percent_failed_errored=2;percent_failed=3"
-	tags="tool_id=0"
+	fields="tool_runs=1;percent_errored=2;percent_failed=3;count_errored=4;count_failed=5"
+	tags="tool_id=0;handler=6"
+
 
 	read -r -d '' QUERY <<-EOF
 		SELECT
 			j.tool_id,
 			count(*) AS tool_runs,
-			sum(CASE WHEN j.state IN ('error', 'failed') THEN 1 ELSE 0 END) * 100.0 / count(*) AS percent_failed_errored,
-			sum(CASE WHEN j.state = 'failed' THEN 1 ELSE 0 END) / count(*) AS percent_failed
+			sum(CASE WHEN j.state = 'error'  THEN 1 ELSE 0 END)::float / count(*) AS percent_errored,
+			sum(CASE WHEN j.state = 'failed' THEN 1 ELSE 0 END)::float / count(*) AS percent_failed,
+			sum(CASE WHEN j.state = 'error'  THEN 1 ELSE 0 END) AS count_errored,
+			sum(CASE WHEN j.state = 'failed' THEN 1 ELSE 0 END) AS count_failed,
+			j.handler
 		FROM job AS j
 		WHERE
 			j.tool_id
@@ -1604,48 +1608,55 @@ query_tool-new-errors() { ## [weeks|4]: Summarize percent of tool runs in error 
 					WHERE j.create_time > (now() - '$weeks weeks'::INTERVAL)
 					GROUP BY j.tool_id
 				)
-		GROUP BY j.tool_id
+		GROUP BY j.tool_id, j.handler
 		ORDER BY percent_failed_errored DESC
 	EOF
 }
 
-query_tool-errors() { ## [weeks|4]: Summarize percent of tool runs in error over the past weeks for all tools that have failed (most popular tools first)
+query_tool-errors() { ## [--short-tool-id] [weeks|4]: Summarize percent of tool runs in error over the past weeks for all tools that have failed (most popular tools first)
 	handle_help "$@" <<-EOF
 		See jobs-in-error summary for recently executed tools that have failed at least 10% of the time.
 
-		    $ gxadmin tool-new-errors 4
-		                                    tool_id                                        | tool_runs | percent_failed_errored | percent_failed
-		    -------------------------------------------------------------------------------+-----------+------------------------+----------------
-		     toolshed.g2.bx.psu.edu/repos/iuc/featurecounts/featurecounts/1.6.3+galaxy2    |      1332 |    10.5105105105105105 |              0
-		     toolshed.g2.bx.psu.edu/repos/iuc/multiqc/multiqc/1.7                          |       937 |    10.1387406616862327 |              0
-		     toolshed.g2.bx.psu.edu/repos/iuc/deseq2/deseq2/2.11.40.6                      |       595 |    32.7731092436974790 |              0
-		     toolshed.g2.bx.psu.edu/repos/iuc/bedtools/bedtools_bedtobam/2.27.0.0          |       573 |    21.6404886561954625 |              0
-		     toolshed.g2.bx.psu.edu/repos/iuc/sra_tools/fastq_dump/2.9.1.3                 |       510 |    15.4901960784313725 |              0
-		     toolshed.g2.bx.psu.edu/repos/iuc/rgrnastar/rna_star/2.6.0b-2                  |       469 |    13.4328358208955224 |              0
-		     toolshed.g2.bx.psu.edu/repos/lparsons/htseq_count/htseq_count/0.9.1           |       429 |    10.4895104895104895 |              0
-		     toolshed.g2.bx.psu.edu/repos/devteam/xy_plot/XY_Plot_1/1.0.2                  |       378 |    12.1693121693121693 |              0
-		     toolshed.g2.bx.psu.edu/repos/iuc/macs2/macs2_callpeak/2.1.1.20160309.5        |       355 |    13.8028169014084507 |              0
-		     toolshed.g2.bx.psu.edu/repos/iuc/datamash_ops/datamash_ops/1.1.0              |       296 |    18.5810810810810811 |              0
-		     toolshed.g2.bx.psu.edu/repos/devteam/vcffilter/vcffilter2/1.0.0_rc1+galaxy2   |       266 |    13.9097744360902256 |              0
-		     toolshed.g2.bx.psu.edu/repos/devteam/fastqtofasta/fastq_to_fasta_python/1.1.1 |       253 |    11.0671936758893281 |              0
+		    $ gxadmin query tool-errors --short-tool-id 1
+		        tool_id                        | tool_runs |  percent_errored  | percent_failed | count_errored | count_failed |     handler
+		    -----------------------------------+-----------+-------------------+----------------+---------------+--------------+-----------------
+		     rnateam/graphclust_align_cluster/ |        55 | 0.145454545454545 |              0 |             8 |            0 | handler_main_10
+		     iuc/rgrnastar/rna_star/2.6.0b-2   |        46 | 0.347826086956522 |              0 |            16 |            0 | handler_main_3
+		     iuc/rgrnastar/rna_star/2.6.0b-2   |        43 | 0.186046511627907 |              0 |             8 |            0 | handler_main_0
+		     iuc/rgrnastar/rna_star/2.6.0b-2   |        41 | 0.390243902439024 |              0 |            16 |            0 | handler_main_4
+		     iuc/rgrnastar/rna_star/2.6.0b-2   |        40 |             0.325 |              0 |            13 |            0 | handler_main_6
+		     Filter1                           |        40 |             0.125 |              0 |             5 |            0 | handler_main_0
+		     devteam/bowtie2/bowtie2/2.3.4.3   |        40 |             0.125 |              0 |             5 |            0 | handler_main_7
+		     iuc/rgrnastar/rna_star/2.6.0b-2   |        40 |               0.3 |              0 |            12 |            0 | handler_main_2
 	EOF
 
-	weeks=${1:-4}
-	fields="tool_runs=1;percent_failed_errored=2;percent_failed=3"
-	tags="tool_id=0"
+	# TODO: Fix this nonsense for proper args
+	tool_id="j.tool_id"
+	if [[ "$1" = --short-tool-id ]]; then
+		tool_id="regexp_replace(j.tool_id, '.*toolshed.*/repos/', '') as tool_id"
+		weeks=${2:-4}
+	else
+		weeks=${1:-4}
+	fi
+
+	fields="tool_runs=1;percent_errored=2;percent_failed=3;count_errored=4;count_failed=5"
+	tags="tool_id=0;handler=6"
 
 	read -r -d '' QUERY <<-EOF
 		SELECT
-			j.tool_id,
+			$tool_id,
 			count(*) AS tool_runs,
-			sum(CASE WHEN j.state IN ('error', 'failed') THEN 1 ELSE 0 END) * 100.0 / count(*) AS percent_failed_errored,
-			sum(CASE WHEN j.state = 'failed' THEN 1 ELSE 0 END) / count(*) AS percent_failed
+			sum(CASE WHEN j.state = 'error'  THEN 1 ELSE 0 END)::float / count(*) AS percent_errored,
+			sum(CASE WHEN j.state = 'failed' THEN 1 ELSE 0 END)::float / count(*) AS percent_failed,
+			sum(CASE WHEN j.state = 'error'  THEN 1 ELSE 0 END) AS count_errored,
+			sum(CASE WHEN j.state = 'failed' THEN 1 ELSE 0 END) AS count_failed,
+			j.handler
 		FROM
 			job AS j
 		WHERE
 			j.create_time > (now() - '$weeks weeks'::INTERVAL)
 		GROUP BY
-			j.tool_id
+			j.tool_id, j.handler
 		HAVING
 			sum(CASE WHEN j.state IN ('error', 'failed') THEN 1 ELSE 0 END) * 100.0 / count(*) > 10.0
 		ORDER BY
@@ -1653,42 +1664,50 @@ query_tool-errors() { ## [weeks|4]: Summarize percent of tool runs in error over
 	EOF
 }
 
-query_tool-likely-broken() { ## [weeks|4]: Find tools that have been executed in recent weeks that are (or were due to job running) likely substantially broken
+query_tool-likely-broken() { ## [--short-tool-id] [weeks|4]: Find tools that have been executed in recent weeks that are (or were due to job running) likely substantially broken
 	handle_help "$@" <<-EOF
 		This runs an identical query to tool-errors, except filtering for tools
 		which were run more than 4 times, and have a failure rate over 95%.
 
-		    $ gxadmin tool-likely-broken 4
-		                                              tool_id                              | tool_runs | percent_failed_errored | percent_failed
-		    -------------------------------------------------------------------------------+-----------+------------------------+----------------
-		     toolshed.g2.bx.psu.edu/repos/iuc/stacks_denovomap/stacks_denovomap/1.46.0     |       133 |    96.2406015037593985 |              0
-		     toolshed.g2.bx.psu.edu/repos/iuc/stacks_populations/stacks_populations/1.46.0 |        49 |    97.9591836734693878 |              0
-		     toolshed.g2.bx.psu.edu/repos/rnateam/dorina/dorina_search/1.0.0               |        44 |   100.0000000000000000 |              0
-		     toolshed.g2.bx.psu.edu/repos/bgruening/diamond/bg_diamond/0.8.24.1            |        43 |   100.0000000000000000 |              0
-		     toolshed.g2.bx.psu.edu/repos/devteam/dna_filtering/histogram_rpy/1.0.3        |        31 |    96.7741935483870968 |              0
-		     toolshed.g2.bx.psu.edu/repos/iuc/bcftools_cnv/bcftools_cnv/1.9                |        30 |   100.0000000000000000 |              0
-		     intermine                                                                     |        26 |   100.0000000000000000 |              0
-		     toolshed.g2.bx.psu.edu/repos/galaxyp/proteomics_moff/proteomics_moff/2.0.2.0  |        23 |   100.0000000000000000 |              0
-		     toolshed.g2.bx.psu.edu/repos/devteam/picard/PicardHsMetrics/1.56.0            |        23 |   100.0000000000000000 |              0
-		     toolshed.g2.bx.psu.edu/repos/iuc/stacks_refmap/stacks_refmap/1.46.0           |        21 |   100.0000000000000000 |              0
+		                             tool_id                       | tool_runs | percent_errored | percent_failed | count_errored | count_failed |     handler
+		    -------------------------------------------------------+-----------+-----------------+----------------+---------------+--------------+-----------------
+		     simon-gladman/velvetoptimiser/velvetoptimiser/2.2.6   |        14 |               1 |              0 |            14 |            0 | handler_main_7
+		     bgruening/hicexplorer_hicplottads/hicexplorer_hicplott|         9 |               1 |              0 |             9 |            0 | handler_main_0
+		     bgruening/text_processing/tp_replace_in_column/1.1.3  |         8 |               1 |              0 |             8 |            0 | handler_main_3
+		     bgruening/text_processing/tp_awk_tool/1.1.1           |         7 |               1 |              0 |             7 |            0 | handler_main_5
+		     rnateam/dorina/dorina_search/1.0.0                    |         7 |               1 |              0 |             7 |            0 | handler_main_2
+		     bgruening/text_processing/tp_replace_in_column/1.1.3  |         6 |               1 |              0 |             6 |            0 | handler_main_9
+		     rnateam/dorina/dorina_search/1.0.0                    |         6 |               1 |              0 |             6 |            0 | handler_main_11
+		     rnateam/dorina/dorina_search/1.0.0                    |         6 |               1 |              0 |             6 |            0 | handler_main_8
 	EOF
 
-	weeks=${1:-4}
-	fields="tool_runs=1;percent_failed_errored=2;percent_failed=3"
-	tags="tool_id=0"
+	# TODO: Fix this nonsense for proper args
+	tool_id="j.tool_id"
+	if [[ "$1" = --short-tool-id ]]; then
+		tool_id="regexp_replace(j.tool_id, '.*toolshed.*/repos/', '') as tool_id"
+		weeks=${2:-4}
+	else
+		weeks=${1:-4}
+	fi
+
+	fields="tool_runs=1;percent_errored=2;percent_failed=3;count_errored=4;count_failed=5"
+	tags="tool_id=0;handler=6"
 
 	read -r -d '' QUERY <<-EOF
 		SELECT
-			j.tool_id,
+			$tool_id,
 			count(*) AS tool_runs,
-			sum(CASE WHEN j.state IN ('error', 'failed') THEN 1 ELSE 0 END) * 100.0 / count(*) AS percent_failed_errored,
-			sum(CASE WHEN j.state = 'failed' THEN 1 ELSE 0 END) / count(*) AS percent_failed
+			sum(CASE WHEN j.state = 'error'  THEN 1 ELSE 0 END)::float / count(*) AS percent_errored,
+			sum(CASE WHEN j.state = 'failed' THEN 1 ELSE 0 END)::float / count(*) AS percent_failed,
+			sum(CASE WHEN j.state = 'error'  THEN 1 ELSE 0 END) AS count_errored,
+			sum(CASE WHEN j.state = 'failed' THEN 1 ELSE 0 END) AS count_failed,
+			j.handler
 		FROM
 			job AS j
 		WHERE
 			j.create_time > (now() - '$weeks weeks'::INTERVAL)
 		GROUP BY
-			j.tool_id
+			j.tool_id, j.handler
 		HAVING
 			sum(CASE WHEN j.state IN ('error', 'failed') THEN 1 ELSE 0 END) * 100.0 / count(*) > 95.0
 			AND count(*) > 4
