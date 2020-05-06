1a8cb318549f36ec21db7541acc0c778ae66d4ce
Anup Kumar <kumara@informatik.uni-freiburg.de>
2020-05-06 18:49:58 +0200

    Update query for collecting workflows
    Fix query and add new columns in a tabular format of workflows used to build a tool recommendation system

--- parts/22-query.sh
+++ parts/22-query.sh
@@ -104,18 +104,18 @@ query_workflow-connections() { ## [--all]: The connections of tools, from output
 		This is used by the usegalaxy.eu tool prediction workflow, allowing for building models out of tool connections in workflows.
 
 		    $ gxadmin query workflow-connections
-		     wf_id |     wf_updated      | in_id |      in_tool      | in_tool_v | out_id |     out_tool      | out_tool_v
-		    -------+---------------------+-------+-------------------+-----------+--------+-------------------+------------
-		         3 | 2013-02-07 16:48:00 |     5 | Grep1             | 1.0.1     |     12 |                   |
-		         3 | 2013-02-07 16:48:00 |     6 | Cut1              | 1.0.1     |      7 | Remove beginning1 | 1.0.0
-		         3 | 2013-02-07 16:48:00 |     7 | Remove beginning1 | 1.0.0     |      5 | Grep1             | 1.0.1
-		         3 | 2013-02-07 16:48:00 |     8 | addValue          | 1.0.0     |      6 | Cut1              | 1.0.1
-		         3 | 2013-02-07 16:48:00 |     9 | Cut1              | 1.0.1     |      7 | Remove beginning1 | 1.0.0
-		         3 | 2013-02-07 16:48:00 |    10 | addValue          | 1.0.0     |     11 | Paste1            | 1.0.0
-		         3 | 2013-02-07 16:48:00 |    11 | Paste1            | 1.0.0     |      9 | Cut1              | 1.0.1
-		         3 | 2013-02-07 16:48:00 |    11 | Paste1            | 1.0.0     |      8 | addValue          | 1.0.0
-		         4 | 2013-02-07 16:48:00 |    13 | cat1              | 1.0.0     |     18 | addValue          | 1.0.0
-		         4 | 2013-02-07 16:48:00 |    13 | cat1              | 1.0.0     |     20 | Count1            | 1.0.0
+		     wf_id |     wf_updated      | in_id |      in_tool      | in_tool_v | out_id |     out_tool      | out_tool_v | published | deleted | has_errors
+		    -------+---------------------+-------+-------------------+-----------+--------+-------------------+----------------------------------------------
+		         3 | 2013-02-07 16:48:00 |     5 | Grep1             | 1.0.1     |     12 |                   |            |    f      |    f    |    f
+		         3 | 2013-02-07 16:48:00 |     6 | Cut1              | 1.0.1     |      7 | Remove beginning1 | 1.0.0      |    f      |    f    |    f
+		         3 | 2013-02-07 16:48:00 |     7 | Remove beginning1 | 1.0.0     |      5 | Grep1             | 1.0.1      |    f      |    f    |    f
+		         3 | 2013-02-07 16:48:00 |     8 | addValue          | 1.0.0     |      6 | Cut1              | 1.0.1      |    t      |    f    |    f
+		         3 | 2013-02-07 16:48:00 |     9 | Cut1              | 1.0.1     |      7 | Remove beginning1 | 1.0.0      |    f      |    f    |    f
+		         3 | 2013-02-07 16:48:00 |    10 | addValue          | 1.0.0     |     11 | Paste1            | 1.0.0      |    t      |    f    |    f
+		         3 | 2013-02-07 16:48:00 |    11 | Paste1            | 1.0.0     |      9 | Cut1              | 1.0.1      |    f      |    f    |    f
+		         3 | 2013-02-07 16:48:00 |    11 | Paste1            | 1.0.0     |      8 | addValue          | 1.0.0      |    t      |    t    |    f
+		         4 | 2013-02-07 16:48:00 |    13 | cat1              | 1.0.0     |     18 | addValue          | 1.0.0      |    t      |    f    |    f
+		         4 | 2013-02-07 16:48:00 |    13 | cat1              | 1.0.0     |     20 | Count1            | 1.0.0      |    t      |    t    |    f
 	EOF
 
 	read -r -d '' wf_filter <<-EOF
@@ -135,19 +135,24 @@ query_workflow-connections() { ## [--all]: The connections of tools, from output
 
 	read -r -d '' QUERY <<-EOF
 		SELECT
-			ws_in.workflow_id as wf_id,
-			workflow.update_time::DATE as wf_updated,
-			ws_in.id as in_id,
-			ws_in.tool_id as in_tool,
-			ws_in.tool_version as in_tool_v,
-			ws_out.id as out_id,
-			ws_out.tool_id as out_tool,
-			ws_out.tool_version as out_tool_v
+                        workflow.id as wf_id,
+                        workflow.update_time as wf_updated,
+                        ws_in.id as in_id,
+                        ws_in.tool_id as in_tool,
+                        ws_in.tool_version as in_tool_v,
+                        ws_out.id as out_id,
+                        ws_out.tool_id as out_tool,
+                        ws_out.tool_version as out_tool_v,
+                        sw.published as published,
+                        sw.deleted as deleted,
+                        workflow.has_errors as has_errors
 		FROM workflow_step_connection wfc
-		LEFT JOIN workflow_step ws_out ON ws_out.id = wfc.output_step_id
+		LEFT JOIN workflow_step ws_in ON ws_in.id = wfc.output_step_id
 		LEFT JOIN workflow_step_input wsi ON wfc.input_step_input_id = wsi.id
-		LEFT JOIN workflow_step ws_in  ON ws_in.id = wsi.workflow_step_id
+		LEFT JOIN workflow_step ws_out ON ws_out.id = wsi.workflow_step_id
+		LEFT JOIN workflow_output as wo ON wsi.workflow_step_id = wfc.output_step_id
 		LEFT JOIN workflow on ws_in.workflow_id = workflow.id
+		LEFT JOIN stored_workflow as sw on sw.latest_workflow_id = workflow.id
 		$wf_filter
 	EOF
 }
