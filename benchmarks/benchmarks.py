cab52bfaf8282930d7e310385b7be6c34a4d6165
Helena Rasche <hxr@hx42.org>
2020-07-28 18:00:03 +0200

    ok subprocess it

---
+++ benchmarks/benchmarks.py
@@ -0,0 +1,9 @@
+# Write the benchmarking functions here.
+# See "Writing benchmarks" in the asv docs for more information.
+import subprocess
+
+
+class GxadminSuite:
+    def time_query_ts_repos(self):
+        gxadmin = '11000acb91c378353a347d6cf4078b40'
+        subprocess.check_output('./gxadmin query ts-repos')
