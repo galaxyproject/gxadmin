91249095445ac65db80f2e8a78a50ff0d4cc8005
Helena Rasche <hxr@hx42.org>
2020-07-28 18:03:21 +0200

    update benchmark

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -6,4 +6,4 @@ import subprocess
 class GxadminSuite:
     def time_query_ts_repos(self):
         gxadmin = 'q1000acb91c378353a347d6cf4078b40'
-        subprocess.check_output('./gxadmin query ts-repos')
+        gxadmin = subprocess.check_output(['/home/hxr/arbeit/galaxy/gxadmin/gxadmin', 'query', 'ts-repos'])
