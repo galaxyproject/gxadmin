156eb419a6ea054407e074bfaf754875798ffc23
Helena Rasche <hxr@hx42.org>
2020-09-07 16:36:47 +0200

    wap updates pulled in

--- parts/70-look-for.sh
+++ parts/70-look-for.sh
@@ -13,7 +13,7 @@ obtain_func() {
 	# ##? enables fancy argument parsing.
 	# Nothing needs to be done, just let that function handle and auto-parse
 	# and auto-export
-	wonderful_argument_parsing "$fn" "$@"
+	wonderful_argument_parser "$fn" "$@"
 
 	if (( ec == 0 )); then
 		$fn "$@";
@@ -22,21 +22,34 @@ obtain_func() {
 	fi
 }
 
-wonderful_argument_parsing() {
-	fn=$1; shift;
+wonderful_argument_parser() {
+	# from the top, the function to look for
+	fn=$1;
+	# make it drop, so we can parse the rest
+	shift;
 	declare -a parsed_keys
 	declare -a parsed_vals
 	declare -a positional_args
-	declare -a flag_args
+	declare -a optional_flag_args
+	declare -a optional_args
 	declare -a args
 	positional_index=0
+	optional_index=0
 
 	# shellcheck disable=SC2207
 	signature=($(grep -s -h -o "${fn}()\s*{ ##?\? .*" "$0" | sed 's/.*##?//g;s/: .*//g;'))
+	signature+=('[--help]') # This is always available
 
 	# shellcheck disable=SC2068
 	for x in $@; do
 		args+=("$x");
+
+		# If the help flag is in there, we can short-circuit
+		if [[ "$x" == "--help" ]] || [[ "$x" == "-h" ]]; then
+			return
+		fi
+
+		# Pop
 		shift;
 	done
 
@@ -44,9 +57,12 @@ wonderful_argument_parsing() {
 		if [[ "$arg" == '<'* ]]; then
 			# This is a required argument
 			positional_args+=("$(echo "$arg" | sed 's/^<//;s/>$//')")
+		elif [[ "$arg" == '[--'* ]]; then
+			# This is an optional argument
+			optional_flag_args+=("$arg")
 		elif [[ "$arg" == '['* ]]; then
 			# This is an optional argument
-			flag_args+=("$arg")
+			optional_args+=("$(echo "$arg" | sed 's/^\[//;s/\]$//')")
 		else
 			# This is an error
 			echo "ERROR!!! Bad argument specification: $arg"
@@ -55,6 +71,7 @@ wonderful_argument_parsing() {
 
 	# Size of the arrays
 	positional_count=${#positional_args[@]}
+	optional_count=${#optional_args[@]}
 
 	offset=0
 	while true; do
@@ -66,7 +83,7 @@ wonderful_argument_parsing() {
 
 		if [[ "$a_cur" == "--"* ]]; then
 			# This is a flag. So find the matching flag definition.
-			for arg in "${flag_args[@]}"; do
+			for arg in "${optional_flag_args[@]}"; do
 				# Two types of args: with, without values
 				if [[ "$arg" == "[--"* ]]; then
 					if [[ "$arg" == *'|'* ]]; then
@@ -89,16 +106,23 @@ wonderful_argument_parsing() {
 				fi
 			done
 		else
-			# This is a non-flag, so a positional argument.
+			# This is a non-flag, so maybe a positional, maybe an optional argument.
 			# So we need to find the Nth positional (via positional_index)
-			if (( positional_index >=  positional_count)); then
-				error "Error: more positional arguments than should be possible"
+			if (( (positional_index + optional_index) >= (positional_count + optional_count) )); then
+				echo "Error: more positional arguments than should be possible"
 				exit 1;
 			fi
 
-			parsed_keys+=("${positional_args[$positional_index]}")
-			parsed_vals+=("${a_cur}")
-			positional_index=$((positional_index + 1))
+			if (( positional_index < positional_count )); then
+				parsed_keys+=("${positional_args[$positional_index]}")
+				parsed_vals+=("${a_cur}")
+				positional_index=$((positional_index + 1))
+			else
+				# We're past the positional and into the optional
+				parsed_keys+=("${optional_args[$optional_index]}")
+				parsed_vals+=("${a_cur}")
+				optional_index=$(( optional_index + 1 ))
+			fi
 		fi
 		offset=$(( offset + 1 ))
 	done
