963ed21d8e5bb996d9ed47c58c8703d63611efba
Gianmauro Cuccuru <gmauro@gmail.com>
2020-10-30 15:08:38 +0100

    Update 05-python.sh
    Enable GXADMIN_PYTHON from the environment

--- parts/05-python.sh
+++ parts/05-python.sh
@@ -1,11 +1,12 @@
 # Find a python binary, hopefully.
-
-if hash python3 2>/dev/null; then
-	export GXADMIN_PYTHON=$(command -v python3)
-elif hash python2 >/dev/null; then
-	export GXADMIN_PYTHON=$(command -v python2)
-elif hash python >/dev/null; then
-	export GXADMIN_PYTHON=$(command -v python)
-else
-	warning "Some features require python support, sorry. If you have python installed somewhere that is not on the path or under a weird name, you can set GXADMIN_PYTHON to the path."
+if [[ -z "${GXADMIN_PYTHON}" ]]; then
+	if hash python3 2>/dev/null; then
+		export GXADMIN_PYTHON=$(command -v python3)
+	elif hash python2 >/dev/null; then
+		export GXADMIN_PYTHON=$(command -v python2)
+	elif hash python >/dev/null; then
+		export GXADMIN_PYTHON=$(command -v python)
+	else
+		warning "Some features require python support, sorry. If you have python installed somewhere that is not on the path or under a weird name, you can set GXADMIN_PYTHON to the path."
+	fi
 fi
