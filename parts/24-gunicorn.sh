7eadd3c0b4fa87e74cca10be2e24bbf653c2ba69
Mira <86979912+mira-miracoli@users.noreply.github.com>
2022-12-21 14:14:45 +0100

    change to tabs

--- parts/24-gunicorn.sh
+++ parts/24-gunicorn.sh
@@ -4,10 +4,11 @@ _server_long_help="
 	Various commands that contain the gunicorn service, such as active users... Please help to extend
 "
 
-server_active-users() { ## : Shows active users in last 10 minutes
+gunicorn_active-users() { ## : Shows active users in last 10 minutes
 	handle_help "$@" <<-EOF
-		cu unique sorts IP adresses from 'GET /history/current_history_json' from last 10 minutes and prints it in influx line format
-        EOF
+		See unique sorts IP adresses from 'GET /history/current_history_json' from last 10 minutes and prints it in influx line format
+	EOF
 	echo "active_users,timespan=last_10_min users=$(journalctl -u galaxy-gunicorn@*.service --since '10 minutes ago' | \
 	grep '/history/current_history_json'  | awk '{print $11}' | sort -u | wc -l)"
 }
+
